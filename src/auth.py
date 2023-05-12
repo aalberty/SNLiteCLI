a1b2e7e2508e52a7640a301bf3de4ad2dff87046
aalberty <46965141+aalberty@users.noreply.github.com>
2023-05-12 11:57:48 +0000

    stubbing token refresh

--- src/auth.py
+++ src/auth.py
@@ -1,1 +1,24 @@
 import requests as r
+import json
+
+file = open('config.json', 'r')
+global config
+config = json.loads(file.read())
+
+#TODO: test refresh_token
+def refresh_token(config):
+    url = config.base_url + "/oauth_token.do"
+    
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": config.refresh_token,
+        "redirect_uri": config.base_url + "/oauth_redirect.do",
+        "client_id": config.client_id,
+        "client_secret": config.client_secret
+    }
+    
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    res = r.post(url, headers=headers, data=data)
